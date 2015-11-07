7b58b4db2bf6b898c4c1003612dd92137c8f5a5b
Lavinia Dan <lavinia.dan@digital-education.com>
2015-11-07 13:35:07 +0200

    check user status in minutes spent on computer
    show notifications if timespent received is larger than default

--- notifications.rb
+++ notifications.rb
@@ -1,2 +1,16 @@
 require 'libnotify'
-Libnotify.show(:body => "hello", :summary => "world", :timeout => 2.5)
+
+class Notification
+  DEFAULT_TIME = 50
+
+  def check_user_status(time_spent)
+    if time_spent > DEFAULT_TIME
+      Libnotify.show(
+          :body    => "Take a walk. You've spent #{time_spent} minutes on your computer",
+          :summary => "You should",
+          :timeout => 2.5
+      )
+    end
+  end
+
+end
