904fe97a3ffd156de3da6a35ef9e6a547ec10b86
Lavinia Dan <lavinia.dan@digital-education.com>
2015-11-07 15:05:38 +0200

    custom notifications examples TOBE IMPROVED

--- notifications.rb
+++ notifications.rb
@@ -1,16 +1,36 @@
 require 'libnotify'
 
 class Notification
-  DEFAULT_TIME = 50
 
-  def check_user_status(time_spent)
-    if time_spent > DEFAULT_TIME
-      Libnotify.show(
-          :body    => "Take a walk. You've spent #{time_spent} minutes on your computer",
-          :summary => "You should",
-          :timeout => 2.5
+  def take_a_walk
+      show_notification(
+          "Take a walk. You've spent 50 minutes on your computer",
+          "You should"
       )
-    end
   end
 
+  def correct_your_legs
+    show_notification(
+        "Correct your legs position",
+        "You should"
+    )
+  end
+
+  def correct_your_back
+    show_notification(
+        "Correct your back position",
+        "You should"
+    )
+  end
+
+  def show_notification(message, summary)
+    Libnotify.show(
+        :body    => message,
+        :summary => summary,
+        :timeout => 2.5
+    )
+  end
 end
+
+# a = Notification.new
+# a.correct_your_legs
